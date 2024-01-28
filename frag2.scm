e03c9d46e60eaaebd11da1e46bcb562e54c1f250
crosscap <wangliankun2018@outlook.com>
2024-01-28 21:22:39 +0800

    start learning scheme and finish ch01 to ch04

---
+++ frag2.scm
@@ -0,0 +1,8 @@
+; hello with name
+(define (hello name)
+  (string-append "Hello " name "!"))
+
+
+; sum of three numbers
+(define (sum3 a b c)
+  (+ a b c))
---
+++ t4-1-1-2.scm
@@ -0,0 +1,3 @@
+(define (inc x) (+ x 1))
+
+(define (dec x) (- x 1))
